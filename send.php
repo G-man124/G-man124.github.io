5d58de98fab35e03754fe27b02b88afd83552b75
G-man124 <graysonogus@gmail.com>
2026-09-08 19:17:34 -0700

    Delete send.php

--- send.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'db.php';
-if (isset($_POST['username']) && isset($_POST['message'])) {
-    $stmt = $pdo->prepare("INSERT INTO messages (username, message) VALUES (?, ?)");
-    $stmt->execute([$_POST['username'], $_POST['message']]);
-}
-?>
